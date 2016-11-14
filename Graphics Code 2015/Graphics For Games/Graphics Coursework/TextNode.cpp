9a333879f655b40a504e398e6d523badd54380f8
Dan Nixon <dan@dan-nixon.com>
2016-11-14 08:56:43 +0000

    add text node

---
+++ Graphics Code 2015/Graphics For Games/Graphics Coursework/TextNode.cpp
@@ -0,0 +1,15 @@
+/** @file */
+
+#include "TextNode.h"
+
+namespace GraphicsCoursework
+{
+  TextNode::TextNode(const std::string & name)
+    : RenderableNode(name)
+  {
+  }
+
+  TextNode::~TextNode()
+  {
+  }
+}
---
+++ Graphics Code 2015/Graphics For Games/Graphics Coursework/TextNode.h
@@ -0,0 +1,17 @@
+/** @file */
+
+#pragma once
+
+#include "RenderableNode.h"
+
+namespace GraphicsCoursework
+{
+  class TextNode : public RenderableNode
+  {
+  public:
+    TextNode(const std::string & name);
+    virtual ~TextNode();
+
+    // TODO
+  };
+}
